11314c660bd90c2bc2d5de417dc2edeb4f9da44b
Adam Gruer <adamgruer@epa.vic.gov.au>
2019-12-04 15:34:34 +1100

    other 3 code

--- 03.R
+++ 03.R
@@ -14,11 +14,19 @@ wire_paths <- scan("03_input.txt", what =character()) %>%
   lapply(function(instructions) {parse_instruction(instructions)})
 
 move <- function(start, direction, distance){
-  switch(direction,
-         U = list())
+        switch(direction,
+               U = list(x = start$x, y = start$y + distance),
+               D = list(x = start$x, y = start$y - distance),
+               R = list(x = start$x + distance, y = start$y),
+               L = list(x = start$x - distance, y = start$y))
   
 }
 
 
+point <- list(x = 0, y = 0)
  
+U7
+
+point2 <-  list(x = point$x, y = point$y + 7)
+move(point, U7)
 
