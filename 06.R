1110e24938293db1146b9d35a793f52e788cd3ae
Adam Gruer <adamgruer@icloud.com>
2019-12-06 20:52:42 +1100

    day6

--- 06.R
+++ 06.R
@@ -1,8 +1,19 @@
 library(magrittr)
-orbits <- scan("06_input.txt", what = character())
-network <- stringr::str_split(orbits, "\\)") %>% 
-  purrr::map_dfr(~ tibble(from = .x[2], to = .x[1]))
+library(tibble)
+library(igraph)
 
-d <- igraph::graph_from_data_frame(network)
+orbits_map <- scan("06_input.txt", what = character())
 
-sum(igraph::distance_table(d)$res)
+orbits_edges <- stringr::str_split(orbits_map, "\\)") %>% 
+  purrr::map_dfr( ~ tibble( from = .x[2],  to = .x[1]))
+
+orbits_graph <- igraph::graph_from_data_frame(orbits_edges)
+
+part1 <- igraph::distance_table(orbits_graph)$res %>% 
+      sum()
+part1
+
+part2 <- distances(orbits_graph, 
+               v = V(orbits_graph)["YOU"] ,
+               to = V(orbits_graph)["SAN"]) - 2
+part2
